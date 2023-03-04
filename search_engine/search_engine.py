e3a43e47b36dc9445499c02eefbada58ab9c8cee
Iris-Raine <iris.raine.darkest.star@gmail.com>
2023-03-04 02:42:25 +0000

    linter error fix

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -16,7 +16,9 @@ def search(documents, query):
             id = document['id']
             result_without_relevance.update([id])
             relevance.setdefault(document['id'], 0)
-            token_relevance = (list(filter(lambda doc: doc['id'] == id, inverted_index[query_token]))[0]['tf-idf'])
+            token_relevance = (list(filter(
+                lambda doc: doc['id'] == id, inverted_index[query_token]
+            ))[0]['tf-idf'])
             relevance[document['id']] += token_relevance
     for id in result_without_relevance:
         result_with_relevance.append(
@@ -31,23 +33,27 @@ def search(documents, query):
 def get_inverted_index(documents):
     inverted_index = {}
     tokens_all = set()
-    documents_as_list_of_tokens = []
+    documents_as_tokens = []
     documents_count = len(documents)
     for document in documents:
         document_tokens = tokenize(document['text'])
-        documents_as_list_of_tokens.append(
+        documents_as_tokens.append(
             {'id': document['id'], 'tokens': document_tokens}
         )
         tokens_all.update(document_tokens)
     for token in tokens_all:
         inverted_index[token] = []
         documents_has_token = len(
-            list(filter(lambda item: token in item['tokens'], documents_as_list_of_tokens)))
+            list(filter(
+                lambda item: token in item['tokens'], documents_as_tokens
+            )))
         idf = log(documents_count / documents_has_token)
-        for document in documents_as_list_of_tokens:
+        for document in documents_as_tokens:
             if token in document['tokens']:
                 tf = document['tokens'].count(token) / len(document['tokens'])
-                inverted_index[token].append({'id': document['id'], 'tf-idf': round(tf * idf, 4)})
+                inverted_index[token].append(
+                    {'id': document['id'], 'tf-idf': round(tf * idf, 4)}
+                )
     return inverted_index
 
 
